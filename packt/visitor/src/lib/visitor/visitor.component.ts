d0800448fb7af6b88c8a40c0c55b8c2fd84393bb
bez <Behzad.p.ashrafi@gmail.com>
2023-12-26 17:35:09 +0300

    update config

---
+++ packt/visitor/src/lib/visitor/visitor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'packt-visitor',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './visitor.component.html',
+  styleUrl: './visitor.component.css',
+})
+export class VisitorComponent {}
